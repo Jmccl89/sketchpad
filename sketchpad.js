261e7b7a396e1aec4f7ad5bb2de342313df05792
James McCloskey <j.mccloskey89@gmail.com>
2015-11-22 14:54:31 -0600

    The grid now changes color as mouse moves over.

--- sketchpad.js
+++ sketchpad.js
@@ -4,6 +4,13 @@ var gridWidth = 960;
 $(document).ready(function(){
 	//$("body").append('<div id="container" style="width:'+gridWidth+'px"></div>'); //fixed size container to hold the grid
 	for (i=1;i<=gridDimension*gridDimension;i++) {
-		$("#container").append('<div class="square" style="height:'+gridWidth/gridDimension+'px; width:'+gridWidth/gridDimension+'px; float:left"></div>');
+		$("#container").append('<div style="height:'+gridWidth/gridDimension+'px; width:'+gridWidth/gridDimension+'px; float:left"></div>');
 	}
+
+	$(document).on('mouseenter', 'div div', function() {
+        $(this).addClass('hovered');
+    });
+    $(document).on('mouseleave', 'div div', function() {
+        $(this).removeClass('hovered');
+    });
 });
